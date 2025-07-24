38ca81a7e6a15f9a5d002321812fb422f74c9ac3
Luciana Yael Romero <lu.yael.r@gmail.com>
2025-07-24 20:16:17 -0300

    cambio de lugar

--- public/includes/header.php
+++ public/includes/header.php
@@ -6,12 +6,13 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <nav>
-        <ul class="menu">
-            <li><a href="index.php">Inicio</a></li>
-            <li><a href="#">1º año</a></li>
-            <li><a href="#">3º año</a></li>
-            <li><a href="#">6º año</a></li>
-            <li><a href="#">Subir Material</a></li>
-        </ul>
-    </nav>
+<nav>
+    <ul class="menu">
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="primero.php">1º año</a></li>
+        <li><a href="tercero.php">3º año</a></li>
+        <li><a href="sexto.php">6º año</a></li>
+        <li><a href="subirMaterial.html">Subir Material</a></li>
+    </ul>
+</nav>
+
---
+++ public/sexto.php
@@ -0,0 +1,9 @@
+<?php include 'includes/header.php'; ?>
+
+<main>
+    <h2>6º Año - Contenidos y Materiales</h2>
+    <p>Acá podés cargar o consultar los materiales por bloque.</p>
+    <!-- A futuro: tabla con los bloques, PDF, links, etc. -->
+</main>
+
+<?php include 'includes/footer.php'; ?>
